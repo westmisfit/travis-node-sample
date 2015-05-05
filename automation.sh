443f565a404f36e4b683c21ab63ec9af2f5c169e
forhot2000 <wx1983@gmail.com>
2015-05-05 17:57:12 +0800

    [skip ci] fix automation.sh

--- automation.sh
+++ automation.sh
@@ -5,12 +5,12 @@ export DOCKER_IMAGE="forhot2000/travis-node-sample"
 # set DOCKER_PUSH false will ignore docker push
 # export DOCKER_PUSH="false"
 
-# overwrite docker run command`
+# overwrite docker run command
 function docker_run () {
     if [[ "$GIT_BRANCH" = "master" ]]; then
-        CONTAINER_ID=$(docker run -d -e NODE_ENV=production ${DOCKER_IMAGE}${IMAGE_TAG})
+        docker run -d -e NODE_ENV=production ${DOCKER_IMAGE}:${IMAGE_TAG}
     elif [[ "$GIT_BRANCH" = "develop" ]]; then
-        CONTAINER_ID=$(docker run -d -e NODE_ENV=staging ${DOCKER_IMAGE}${IMAGE_TAG})
+        docker run -d -e NODE_ENV=staging ${DOCKER_IMAGE}:${IMAGE_TAG}
     fi
 }
 
